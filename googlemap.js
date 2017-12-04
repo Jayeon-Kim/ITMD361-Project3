e51573010575b54127a400de408f2ec9815d905b
Jayeon-Kim <31637995+Jayeon-Kim@users.noreply.github.com>
2017-12-03 22:32:59 -0600

    chagne js
    chagne js

--- googlemap.js
+++ googlemap.js
@@ -1,11 +1,11 @@
 function initMap() {
-  var uluru = {lat : 37.566535, lng:126.977969};
+  var home = {lat : 37.566535, lng:126.977969};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom :6,
-    center : uluru
+    center : home
   });
   var marker = new google.maps.Marker({
-    position: uluru,
+    position: home,
     map : map
   });
 }
