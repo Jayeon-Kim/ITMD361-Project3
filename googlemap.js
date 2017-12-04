e08e9e27df7fdbd683a4ceab59e10ec423fd9b76
Jayeon-Kim <31637995+Jayeon-Kim@users.noreply.github.com>
2017-12-03 22:50:41 -0600

    change js3
    change js3

--- googlemap.js
+++ googlemap.js
@@ -1,11 +1,21 @@
 function initMap() {
-  var home = {lat : 37.566535, lng:126.977969};
+  var seoul = {lat : 37.566535, lng:126.977969};
+  var home = {lat : 37.478487, lng : 126.864288};
   var map = new google.maps.Map(document.getElementById('map'), {
+    zoom :7,
+    center : seoul
+  });
+   var map = new google.maps.Map(document.getElementById('map'), {
     zoom :7,
     center : home
   });
   var marker = new google.maps.Marker({
+    position: seoul,
+    map : map
+  });
+  var marker2 = new google.maps.Marker({
     position: home,
     map : map
   });
+  
 }
