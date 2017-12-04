5a56da79710e613257920578d3ac0516bedf8cee
Jayeon-Kim <31637995+Jayeon-Kim@users.noreply.github.com>
2017-12-03 22:33:58 -0600

    chagne js2
    chagne js2

--- googlemap.js
+++ googlemap.js
@@ -1,7 +1,7 @@
 function initMap() {
   var home = {lat : 37.566535, lng:126.977969};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom :6,
+    zoom :7,
     center : home
   });
   var marker = new google.maps.Marker({
